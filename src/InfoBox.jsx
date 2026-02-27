441d1772faf9e69060cde59576d43f73662d5a61
mishra-khushboo <khush27093@gmail.com>
2026-02-27 17:30:32 +0530

    added InfoBox

---
+++ src/InfoBox.jsx
@@ -0,0 +1,39 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css";
+export default function InfoBox() {
+	const INIT_URL = "https://images.unsplash.com/photo-1641970304222-b2b332808a4b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZHVzdHklMjB3ZWF0aGVyfGVufDB8fDB8fHww"
+	let info = {
+		city: "Mumbai",
+		feelslike: 24.84,
+		temp: 25.06,
+		tempMin: 24.48,
+		tempMax: 26.89,
+		humidity: 47,
+		weather: "haze"
+	};
+
+	return (<div className="InfoBox"><h2>Weather Info - {info.weather}</h2>
+		<Card sx={{ maxWidth: 345 }}>
+			<CardMedia
+				sx={{ height: 140 }}
+				image={INIT_URL}
+				title="green iguana"
+			/>
+			<CardContent>
+				<Typography gutterBottom variant="h5" component="div">
+					{info.city}
+				</Typography>
+				<Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
+					<p>Temperature = {info.temp}&deg;C</p>
+					<p>Humidity = {info.humidity}</p>
+					<p>Min Temp = {info.tempMin}&deg;C</p>
+					<p>Max Temp = {info.tempMax}&deg;C</p>
+					<p>The weather can be described as <i>{info.weather}</i> and The weather feels like {info.feelslike}&deg;C</p>
+				</Typography>
+			</CardContent>
+		</Card>
+	</div>)
+}
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -12,8 +12,15 @@ export default function SearchBox() {
 		let jsonResponse = await response.json();
 		console.log(jsonResponse);
 		let result = {
-			temp: jsonResponse.main.temp
-		}
+			city: "Mumbai",
+			temp: jsonResponse.main.temp,
+			tempMin: jsonResponse.main.temp_min,
+			tempMax: jsonResponse.main.temp_max,
+			humidity: jsonResponse.main.humidity,
+			feelslike: jsonResponse.main.feels_like,
+			weather: jsonResponse.weather[0].description,
+		};
+		console.log(result);
 	}
 
 	let [city, setCity] = useState("");
